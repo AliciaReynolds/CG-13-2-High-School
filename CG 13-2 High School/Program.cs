84b98c76348a88a4b25e9f06af4c4dbe04c1299b
Alicia Reynolds <alicia.a.wilson@gmail.com>
2018-10-07 00:01:16 -0500

    Add project files.

---
+++ CG 13-2 High School/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CG_13_2_High_School
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
